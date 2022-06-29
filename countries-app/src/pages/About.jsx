e87c1cd7e8325644fd4c55fd306183988d04c580
Tania Vervier <tvervier@gmail.com>
2022-06-29 16:54:52 +0200

    tuto almost finished

---
+++ countries-app/src/pages/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>About</h1>
+            <br />
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem corrupti consequatur, excepturi iste iure dicta eveniet. Quia a nobis sapiente enim dolorem cumque sint fuga ad, accusantium autem, odio aspernatur! Aliquam omnis iure ab fugit rem quidem aspernatur eaque labore quam ex at, odio fuga pariatur modi tempora eum voluptatum, architecto minima. Eum quo odit dolore soluta ea aliquam expedita veritatis maxime unde fugiat sit excepturi nemo adipisci aut similique porro minima omnis explicabo ipsum fuga ullam, dolorum veniam deleniti rerum! Harum magnam, iusto, tenetur rerum doloremque quas, fugit rem repudiandae consectetur alias eaque exercitationem dolores delectus deserunt? Unde, modi.</p>
+            <br />
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam eum maiores aut cum ducimus quis minus corporis, maxime eveniet voluptatibus quidem nam ipsam et est rem beatae quas perspiciatis, unde voluptates. Officiis aperiam facilis aspernatur labore neque, minima, quibusdam adipisci repellat itaque reiciendis explicabo cum nesciunt aliquam fugiat tenetur nam repellendus odio ratione quia quae eaque cupiditate laudantium. Delectus, temporibus. Mollitia, eligendi dicta suscipit, dolore consequatur, quisquam ullam veniam provident nisi libero velit quis laboriosam consectetur aliquid dolorum rerum alias obcaecati! Vitae, nostrum ex! Magnam aspernatur, illo, deserunt, aut quisquam ex ad nostrum labore accusantium laborum a delectus provident voluptates.</p>
+        </div>
+    );
+};
+
+export default About;
